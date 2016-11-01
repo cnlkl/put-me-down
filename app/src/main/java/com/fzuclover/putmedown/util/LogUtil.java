38a2b4bf96c9714c4bd2ea00a1e7c7ad5a5ba3cc
cnlkl <nowlinlei@outlook.com>
2016-11-01 19:24:43 +0800

    建立整体框架

---
+++ app/src/main/java/com/fzuclover/putmedown/util/LogUtil.java
@@ -0,0 +1,44 @@
+package com.fzuclover.putmedown.util;
+
+import android.util.Log;
+
+/**
+ * Created by lkl on 2016/11/1.
+ */
+
+public class LogUtil {
+    //分别对应不同Log等级
+    private static final int VERBOSE = 1;
+    private static final int DEBUG = 2;
+    private static final int INFO = 3;
+    private static final int WARN = 4;
+    private static final int ERROR = 5;
+    private static final int NOTHING = 6;
+    private static final int LEVEL = VERBOSE;//用于控制Log打印不同等级的信息
+
+    public static void v(String tag, String msg) {
+        if (LEVEL <= VERBOSE) {
+            Log.v(tag, msg);
+        }
+    }
+    public static void d(String tag, String msg) {
+        if (LEVEL <= DEBUG) {
+            Log.d(tag, msg);
+        }
+    }
+    public static void i(String tag, String msg) {
+        if (LEVEL <= INFO) {
+            Log.i(tag, msg);
+        }
+    }
+    public static void w(String tag, String msg) {
+        if (LEVEL <= WARN) {
+            Log.w(tag, msg);
+        }
+    }
+    public static void e(String tag, String msg) {
+        if (LEVEL <= ERROR) {
+            Log.e(tag, msg);
+        }
+    }
+}
