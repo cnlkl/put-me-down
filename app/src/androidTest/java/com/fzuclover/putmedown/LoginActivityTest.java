a8b052e33c408835239d255a352c324c8b64ca03
cnlkl <nowlinlei@outlook.com>
2016-12-09 23:48:11 +0800

    添加登入按钮测试类

---
+++ app/src/androidTest/java/com/fzuclover/putmedown/LoginActivityTest.java
@@ -0,0 +1,38 @@
+package com.fzuclover.putmedown;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.fzuclover.putmedown.features.login.LoginActivity;
+
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by lkl on 2016/12/9.
+ */
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<LoginActivity>(LoginActivity.class);
+
+    @Test
+    public void loginTest(){
+        onView(withId(R.id.login)).perform(click());
+    }
+
+}
