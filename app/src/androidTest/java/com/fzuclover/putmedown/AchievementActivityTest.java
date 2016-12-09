c483c2f71f26431550b6d6ebef7371ff9628fbd5
Orlando17 <1041230983@qq.com>
2016-12-10 00:34:16 +0800

    测试

---
+++ app/src/androidTest/java/com/fzuclover/putmedown/AchievementActivityTest.java
@@ -0,0 +1,53 @@
+package com.fzuclover.putmedown;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.fzuclover.putmedown.features.achievement.AchievementActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+/**
+ * Created by Administrator on 2016/12/8.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class AchievementActivityTest  {
+
+
+    @Rule
+    public ActivityTestRule<AchievementActivity> mActivityRule = new ActivityTestRule<AchievementActivity>(
+            AchievementActivity.class);
+
+   public  AchievementActivityTest()
+    {
+
+    }
+
+
+    @Test
+    public void testPreconditions()throws Exception  {
+        // @Test注解表示一个测试用例方法
+        onView(withText("累计时间")).check(matches(isDisplayed()));
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
